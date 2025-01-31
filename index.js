1183925fd847c9e60857516a0928c18156fb7a7e
letsi <daniel.lau67@yahoo.de>
2025-01-31 19:27:56 +0100

    Added automatic installation of BepInEx

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
 const path = require('path');
-const { fs, log, util } = require('vortex-api');
+const { fs, util } = require('vortex-api');
 
 const STEAMAPP_ID = '2890830';
 const GAME_ID = 'streamerlifesimulator2';
 const BEPINEX_PATH = 'BepInEx';
+const BEPINEX_PLUGIN_PATH = 'plugins';
 
 function main(context) {
+    context.requireExtension('modtype-bepinex');
     context.registerGame({
         id: GAME_ID,
         name: 'Streamer Life Simulator 2',
+        logo: 'gameart.jpg',
         mergeMods: true,
         queryPath: findGame,
-        supportedTools: [],
-        queryModPath: () => '',
-        logo: 'gameart.jpg',
+        queryModPath: () => path.join(BEPINEX_PATH, BEPINEX_PLUGIN_PATH),
         executable: () => 'Streamer Life Simulator 2.exe',
+        setup,
         requiredFiles: [
             'Streamer Life Simulator 2.exe',
-			'UnityPlayer.dll',
+            'UnityPlayer.dll',
         ],
-        setup: prepareForModding,
         environment: {
             SteamAPPId: STEAMAPP_ID,
         },
@@ -27,7 +28,29 @@ function main(context) {
             steamAppId: STEAMAPP_ID,
         },
     });
-    return true;
+
+    context.once(() => {
+        if (context.api.ext.bepinexAddGame !== undefined) {
+            context.api.ext.bepinexAddGame({
+                gameId: GAME_ID,
+                autoDownloadBepInEx: true,
+                customPackDownloader: () => {
+                    return {
+                        gameId: GAME_ID,
+                        domainId: GAME_ID,
+                        modId: '1',
+                        fileId: '1',
+                        archiveName: 'BepInEx Pack - Streamer Life Simulator 2.zip',
+                        allowAutoInstall: true,
+                    };
+                },
+                doorstopConfig: {
+                    doorstopType: 'default',
+                    ignoreDisableSwitch: true,
+                }
+            });
+        }
+    });
 }
 
 function findGame() {
@@ -35,17 +58,25 @@ function findGame() {
         .then(game => path.join(game.gamePath, "windows_content"));
 }
 
-async function prepareForModding(discovery) {
-    const modPaths = [
-        path.join(discovery.path, BEPINEX_PATH)
-    ];
-    try {
-        await Promise.all(modPaths.map((m) => fs.ensureDirWritableAsync(m)));
-        return Promise.resolve();
-    } catch (err) {
-        log('error', 'Failed to prepare for modding', err);
-        return Promise.reject(err);
-    }
+function setup(discovery) {
+    const pluginsPath = path.join(discovery.path, BEPINEX_PATH, BEPINEX_PLUGIN_PATH);
+
+    return fs.statAsync(pluginsPath)
+        .then(stat => {
+            if (!stat.isDirectory()) {
+                throw new Error(`${pluginsPath} exists but is not a directory`);
+            }
+        })
+        .catch(err => {
+            if (err.code === 'ENOENT') {
+                return fs.ensureDirAsync(pluginsPath);
+            } else {
+                throw err;
+            }
+        })
+        .then(() => {
+            return fs.ensureDirWritableAsync(pluginsPath);
+        });
 }
 
 module.exports = {
